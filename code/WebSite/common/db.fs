5ee661aeeb6923d8200f7c386e8bd81d29440ba8
Tim Forkmann <tforkmann@gmail.com>
2016-08-22 11:06:22 +0200

    initial commit

--- code/WebSite/common/db.fs
+++ code/WebSite/common/db.fs
@@ -6,18 +6,3 @@ open MinimalFableSuave.Config
 open FSharp.Linq
 open System.Data.SqlClient
 open FSharp.Reflection
-
-// #if NODB
-// let [<Literal>] CompileTimeConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SageEnMS;Integrated Security=True"
-// #else
-
-// let [<Literal>] CompileTimeConnectionString = "Data Source=SQLDG\DG;Initial Catalog=SageEnMS;User ID=enmsreporting;password=re6Pheix"
-
-// let [<Literal>] ResolutionPath = __SOURCE_DIRECTORY__
-
-// #endif
-
-// type Sql = SqlDataProvider<ConnectionString = CompileTimeConnectionString, UseOptionTypes=true>
-// let triggerSQL = Common.QueryEvents.SqlQueryEvent |> Observable.subscribe(printfn "Excecuting SQL: %s")
-
-// let db = Sql.GetDataContext connectionString
--- code/WebSite/common/domain.fs
+++ code/WebSite/common/domain.fs
@@ -1,33 +1,5 @@
 module MinimalFableSuave.Domain
 
-// Mini Query
-
-//type Queryelements =
-//  { Bezeichnung : string;
-//    Strasse : string }
-
-// Energyplants
-
-#if NODB 
-    type Heatingnets =
-  { BaujahrTrasse : string;  
-    Energietraeger : string ;
-    Netzeigentuemer : string ;
-    Verlegungsart : string ;
-    Bezeichnung : string;
-    Material : string }
-#endif
-
-type Measures =
-  { Saktobez : string;
-    Kststelle : string ;
-    Betrag : decimal ;
-    Menge : float ;
-    Einheit : string ;
-    VUPeriode : int }
-
-// Optimizationsfeatures
-
 let formatOption defaultValue x = defaultArg (x |> Option.map (fun x -> x.ToString())) defaultValue
 
 let formatNotAvailable x = formatOption "(Nicht vorhanden)" x
